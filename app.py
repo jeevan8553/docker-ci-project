b2ce6402172dfc341d1fe8cd264ef43ecdb90669
Jeevan8553 <jeevanbadduri8553@gmail.com>
2026-05-20 11:26:03 +0530

    updated webhook test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Build Test Version X"
+    return "CI/CD UPDATED SUCCESSFULLY 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
