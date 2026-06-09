761119608937d59674a18bfadb08890103533b68
Jeevan8553 <jeevanbadduri8553@gmail.com>
2026-06-09 19:36:35 +0530

    Updated API response

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD UPDATED SUCCESSFULLY-12345"
+    return "CI/CD UPDATED SUCCESSFULLY!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
